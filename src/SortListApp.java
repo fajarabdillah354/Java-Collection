921c41278fcf4a02719572c4254e0d992b9d8618
fajar abdillah ahmad <fajardillah25@gmail.com>
2022-09-23 17:15:52 +0700

    membuat class implements Comparator

---
+++ src/SortListApp.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+
+public class SortListApp implements Comparator<Integer> {
+
+
+  @Override
+  public int compare(Integer o1, Integer o2) {
+    return -o1.compareTo(o2);
+  }
+}
