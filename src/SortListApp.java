a07a57f1dd7e9541a1b42f141c4bf7d2382f29de
fajar abdillah ahmad <fajardillah25@gmail.com>
2022-09-25 13:31:53 +0700

    latihan List

--- src/SortListApp.java
+++ src/SortListApp.java
@@ -5,6 +5,6 @@ public class SortListApp implements Comparator<Integer> {
 
   @Override
   public int compare(Integer o1, Integer o2) {
-    return -o1.compareTo(o2);
+    return o1.compareTo(o2);
   }
 }
---
+++ src/latihan/ListLingkarang.java
@@ -0,0 +1,23 @@
+package latihan;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListLingkarang {
+  public static void main(String[] args) {
+    List<Object> objectList = new ArrayList<>();
+    objectList.add("jari-jari = 100");
+    objectList.add("Phi = 3,14");
+    objectList.add("diameter = 200");
+    System.out.println(objectList);
+
+    objectList.size();
+
+    System.out.println(objectList.size());
+
+
+
+  }
+
+
+}
