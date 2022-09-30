62232c4d2cf299d32846b694ee2711be38b50a84
fajar abdillah ahmad <fajardillah25@gmail.com>
2022-09-30 14:48:35 +0700

    person class

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Person {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+
+  public Person(String name) {
+    this.name = name;
+    this.barang = new ArrayList<>();
+  }
+
+  @Override
+  public String toString() {
+    return "Person{" +
+            "name='" + name + '\'' +
+            '}';
+  }
+
+  private String name;
+
+  private List<Object> barang;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public List<Object> getBarang() {
+    return barang;
+  }
+
+  public void setBarang(List<Object> barang) {
+    this.barang = barang;
+  }
 }
