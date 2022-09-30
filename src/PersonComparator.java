07f8c7e652d398ba4f16cec5441991beb0c38e27
fajar abdillah ahmad <fajardillah25@gmail.com>
2022-09-30 14:50:07 +0700

    personComparator

--- src/PersonComparator.java
+++ src/PersonComparator.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class PersonComparator {
+import java.util.Comparator;
+
+public class PersonComparator implements Comparator<Person> {
+  @Override
+  public int compare(Person o1, Person o2) {
+    return o1.getName().compareTo(o2.getName());
+  }
 }
