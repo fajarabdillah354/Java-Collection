e19c0faaca60d2013dbfe8bdb77e2659c2d509eb
fajar abdillah ahmad <fajardillah25@gmail.com>
2022-10-05 23:44:30 +0700

    latihan deque(penambahan method)

--- src/StackApp.java
+++ src/StackApp.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class StackApp {
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class StackApp {
+
+  public static void main(String[] args) {
+    /*
+    saat menggunakan deque kita bisa melakukan first in first out ataupun last in first out
+    dalam artian bisa mengambil data dari belakang maupun depan
+     */
+    Deque<String> stack = new ArrayDeque<>();
+
+    stack.offerLast("fajar");
+    stack.offerLast("nisa");
+    stack.offerLast("ujeh");
+
+    System.out.println("=========ini data yang tersedia ===========");
+    for(var value : stack){
+      //mencetak data yang ada
+      System.out.println(value);
+    }
+
+    System.out.println("========= mengambil data dari belakang(stack) dari data =========");
+    //poll digunakan untuk mengambil data,dibawah ini kita mengambil data pertama dari data terakhir
+    System.out.println(stack.pollLast());
+    System.out.println(stack.pollLast());
+    System.out.println(stack.pollLast());
+
+
+    System.out.println("========== mengambil data dari depan(antrian) ===========");
+    System.out.println(stack.pollFirst());
+    System.out.println(stack.pollFirst());
+    System.out.println(stack.pollFirst());
+
+  }
+
+
 }
