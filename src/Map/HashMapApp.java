6887477644826ba2b0dbcd429896ab24168623e0
fajar abdillah ahmad <fajardillah25@gmail.com>
2022-10-07 10:08:10 +0700

    latihan Map(HashMap)

--- src/Map/HashMapApp.java
+++ src/Map/HashMapApp.java
@@ -1,2 +1,36 @@
-package Map;public class HashMapApp {
+package Map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapApp {
+
+  public static void main(String[] args) {
+
+    /*
+    * HashMap mendistribusikan KEY menggunakan    hashcode function
+    * KEY yang ada tidak boleh diplicate.ini karna pengecekannya menggunakan Equals()
+     */
+    Map<String,Integer> uas = new HashMap<>();
+    System.out.println("===== nama-nama peserta ujian CPNS ======");
+    //       KEY,VALUE
+    uas.put("Cemen Simanjutak",96);
+    uas.put("Sujdono",92);
+    uas.put("Kakek Sugiono",80);
+    uas.put("Ucup Jumiancup",90);
+
+    for (var value : uas.keySet()){
+      System.out.println(value);
+    }
+
+    System.out.println("==== nilai tertinggi ujian CPNS ====");
+    for(var nilai : uas.values()){
+      System.out.println(nilai);
+    }
+    System.out.println("==========================");
+    var nilaiCemen = uas.get("Cemen Simanjutak");
+    System.out.println("nilai Cemen : "+nilaiCemen);//jika value tidak sesuai dengan yang ada maka akan mengembalikan nilai null
+
+  }
+
 }
