136fd8f77179268d530d730ba52ab0b9ff0f9e34
fajar abdillah ahmad <fajardillah25@gmail.com>
2022-10-08 22:48:08 +0700

    latihan Map

--- src/Map/SortedMapApp.java
+++ src/Map/SortedMapApp.java
@@ -1,2 +1,26 @@
-package Map;public class SortedMapApp {
+package Map;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class SortedMapApp {
+  /*
+  konkrit implementasi dari sorted Map adalah class TreeMap,di bawah ini tanpa menggunakan Comparator
+   */
+
+  public static void main(String[] args) {
+    SortedMap<String,Integer> dateSiswa = new TreeMap<>();
+
+    dateSiswa.put("Fajar Abdillah Ahmad",21);
+    dateSiswa.put("Siti Anisah",20);
+    dateSiswa.put("Anak Singkong",19);
+    dateSiswa.put("Radjimana Rajin",15);
+    //Ketika di cetak maka akan berurut sesuat urutan alfabet (yang diurutkan terlebih dahulu adalah KEYnya baru VALUEnya
+    for (var value : dateSiswa.keySet()){
+      System.out.println(value);
+    }
+
+    System.out.println(dateSiswa.values());
+    System.out.println(dateSiswa.get("Anak Singkong"));
+  }
 }
